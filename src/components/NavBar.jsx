2401c0ec4014e00e7521fef3dd75b155c00e3dce
Norberto Rubulotta <nrubulotta24@gmail.com>
2022-06-15 14:55:39 +0200

    Se crea react app

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const NavBar = () => {
+    return (
+<ul>
+    <li><a></a></li>
+    <li><a></a></li>
+    <li><a></a></li>
+    <li><a></a></li>
+</ul>
+
+
+    )
+}
